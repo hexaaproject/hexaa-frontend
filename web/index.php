ef836cc6360b07749413ca84aa7ba4075114110b
merlin <merlin@irene>
2016-11-11 15:26:07 +0100

    img and css

--- web/index.php
+++ web/index.php
@@ -16,8 +16,18 @@
  * limitations under the License.
  */
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$loader = new Twig_Loader_Filesystem('../views/');
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/../views/');
 $twig = new Twig_Environment($loader);
 
+$template = $twig->loadTemplate('startpage.html.twig');
+
+$eppn = filter_input(INPUT_SERVER,"eppn");
+
+if (!$eppn){
+    // TODO error handling nicely
+    trigger_error("No eppn value found.", E_ERROR);
+} 
+
+echo $template->render(array('user' => ['eppn' => $eppn]));
