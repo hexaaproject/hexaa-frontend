416f83ec5915afadbdd356cf3a0c6e5fd7b1d24b
Annamária Nagy <nagy.annamaria@sztaki.mta.hu>
2016-11-21 13:30:08 +0100

    Add some JS

---
+++ getOrganizations.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+<script src="jquery-3.1.1.min.js"></script>
+<script>
+    $.ajax({
+        type: "GET",
+        url: url,
+        dataType: json,
+        success: function(data){
+            
+        }
+    });       
+</script>
+
