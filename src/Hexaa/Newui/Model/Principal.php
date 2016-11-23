8ff624de63e70a28f676a180e55e9bb07f6d98a5
solazs <soltesz.balazs.91@gmail.com>
2016-11-23 11:04:28 +0100

    Principal class rework

--- src/Hexaa/Newui/Model/Principal.php
+++ src/Hexaa/Newui/Model/Principal.php
@@ -7,43 +7,7 @@ namespace Hexaa\Newui\Model;
  * Date: 2016.11.11.
  * Time: 16:02
  */
-class Principal
+class Principal extends BaseResource 
 {
-    private $eppn;
-    private $displayName;
-    private $email;
-
-    public function __construct(string $eppn, string $displayName, string $email)
-    {
-        $this->eppn = $eppn;
-        $this->displayName = $displayName;
-        $this->email = $email;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEppn(): string
-    {
-        return $this->eppn;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDisplayName(): string
-    {
-        return $this->displayName;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-
-
+    protected static $pathName = 'principals';
 }
