79169a313ba87361be312262df9996f17e417660
solazs <soltesz.balazs.91@gmail.com>
2016-11-23 11:39:24 +0100

    added pagination to baseResource

--- src/Hexaa/Newui/Model/BaseResource.php
+++ src/Hexaa/Newui/Model/BaseResource.php
@@ -14,8 +14,12 @@ use GuzzleHttp\Client;
 abstract class BaseResource
 {
     protected static $pathName;
-    public static function cget(Client $client) {
-        $response = $client->get(static::$pathName);
+    public static function cget(Client $client, int $offset = 0, int $pageSize = 25) {
+        $response = $client->get(static::$pathName,[
+            'query' => [
+                'offset' => $offset,
+                'limit' => $pageSize
+        ]]);
         return json_decode($response->getBody(), true);
     }
 }
