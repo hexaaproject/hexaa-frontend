03184258e37e32fc8c6542faa8b6dbad0d23152e
merlin <merlin@irene>
2016-11-25 14:55:52 +0100

    service css in separate file

---
+++ web/service.php
@@ -0,0 +1,40 @@
+<?php
+
+/* 
+ * Copyright 2016 Mihály Héder <mihaly.heder@sztaki.mta.hu>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+include_once '../initpage.php';
+
+try {
+    $serviceid = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    if ($serviceid) {
+        $service = \Hexaa\Newui\Model\Service::get($client, $serviceid);
+    }
+} catch (ClientException $e) {
+    $this->token = null;
+    $templateerror = $twig->loadTemplate('error.html.twig');
+    echo $templateerror->render(array('clientexception' => $e));
+} catch (ServerException $e) {
+    $this->token = null;
+    $templateerror = $twig->loadTemplate('error.html.twig');
+    echo $templateerror->render(array('serverexception' => $e));
+} finally {
+    
+}
+
+$template = $twig->loadTemplate('servicemain.html.twig');
+
+echo $template->render(array('user' => $user, 'service' => $service));
