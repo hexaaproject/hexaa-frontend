b7c1f010947fac75317932c1d332d752bd045e7d
Annamária Nagy <nagy.annamaria@sztaki.mta.hu>
2017-01-12 15:21:59 +0100

    Modified menu

--- app/web/app_dev.php
+++ app/web/app_dev.php
@@ -7,7 +7,6 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
 // for more information
 //umask(0000);
-
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 // if (isset($_SERVER['HTTP_CLIENT_IP'])
@@ -19,7 +18,7 @@ use Symfony\Component\Debug\Debug;
 // }
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require __DIR__ . '/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
