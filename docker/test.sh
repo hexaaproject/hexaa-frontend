a9f016e08eea35c2f0d54229bd368424185d5a6d
Szabó Gyula <gyufi@szabocsalad.com>
2017-01-17 17:20:57 +0100

    na vajon loginol-e pushol-e a docker registrybe?

--- docker/test.sh
+++ docker/test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd /app
+cd /var/www/project
 # populate test datas
 #app/console h:d:f:l -n
 echo "check environment"
 ping -c 5 grid
 ping -c 5 hexaa-backend
-/var/www/project/test.sh
+/var/www/project/test.sh /var/www/project
