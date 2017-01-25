1655a94a2f23f9cbdf8fd2b6b63a0206d10941a4
Szabó Gyula <gyufi@szabocsalad.com>
2017-01-25 15:13:55 +0100

    no nézzük, hogy fut le

--- tester_docker_starter.sh
+++ tester_docker_starter.sh
@@ -5,13 +5,16 @@ docker rm hexaa-backend -f
 docker rm grid -f 
 docker rm hexaa-test-data-manager -f
 docker rm project.local -f
+docker network rm hexaa-test
 
-docker run -d --name db -e MYSQL_ALLOW_EMPTY_PASSWORD=true -e MYSQL_USER=someuser -e MYSQL_PASSWORD=somepass -e MYSQL_DATABASE=hexaa mysql
-docker run -d --name memcached memcached
-docker run -d --name grid -p 5900:25900 --add-host project.local:172.17.0.1 elgalu/selenium
-docker run -d --name hexaa-backend --link db:db --link memcached:memcached hexaaproject/hexaa-backend:for-dev
-docker run -d --name hexaa-test-data-manager --link db:db --link hexaa-backend:hexaa-backend hexaaproject/hexaa-test-data-manager:for-dev
-docker run -d --name project.local -p 80:80 -p 443:443 -p 8080:8080 --add-host project.local:127.0.0.1 --link grid:grid --link hexaa-test-data-manager:hexaa-test-data-manager --link hexaa-backend:hexaa-backend solazs/hexaa-newui
+
+docker network create hexaa-test
+docker run -d --name db --network=hexaa-test -e MYSQL_ALLOW_EMPTY_PASSWORD=true -e MYSQL_USER=someuser -e MYSQL_PASSWORD=somepass -e MYSQL_DATABASE=hexaa mysql
+docker run -d --name memcached --network=hexaa-test memcached
+docker run -d --name grid --network=hexaa-test elgalu/selenium
+docker run -d --name hexaa-backend --network=hexaa-test hexaaproject/hexaa-backend:for-dev
+docker run -d --name hexaa-test-data-manager --network=hexaa-test hexaaproject/hexaa-test-data-manager:for-dev
+docker run -d --name project.local --network=hexaa-test solazs/hexaa-newui
 
 sleep 18
 docker exec -t project.local /var/www/project/test.sh /var/www/project
