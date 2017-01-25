d1557731889a056418714c721c0031a77c404706
Szabó Gyula <gyufi@szabocsalad.com>
2017-01-25 15:03:36 +0100

    látják egymást

--- tester_docker_starter.sh
+++ tester_docker_starter.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-docker rm db
-docker rm memcached
-docker rm hexaa-backend
-docker rm grid
-docker rm hexaa-test-data-manager
+docker rm db -f 
+docker rm memcached -f 
+docker rm hexaa-backend -f 
+docker rm grid -f 
+docker rm hexaa-test-data-manager -f
+docker rm project.local -f
 
 docker run -d --name db -e MYSQL_ALLOW_EMPTY_PASSWORD=true -e MYSQL_USER=someuser -e MYSQL_PASSWORD=somepass -e MYSQL_DATABASE=hexaa mysql
 docker run -d --name memcached memcached
+docker run -d --name grid -p 5900:25900 --add-host project.local:172.17.0.1 elgalu/selenium
 docker run -d --name hexaa-backend --link db:db --link memcached:memcached hexaaproject/hexaa-backend:for-dev
-docker run -d --name grid elgalu/selenium
-docker run -d --name hexaa-test-data-manager --link hexaa-backend:hexaa-backend hexaaproject/hexaa-test-data-manager:for-dev
-docker run --rm --link db:db --link grid:grid --link hexaa-test-data-manager:hexaa-test-data-manager -t solazs/hexaa-newui /var/www/project/test.sh /var/www/project
+docker run -d --name hexaa-test-data-manager --link db:db --link hexaa-backend:hexaa-backend hexaaproject/hexaa-test-data-manager:for-dev
+docker run -d --name project.local -p 80:80 -p 443:443 -p 8080:8080 --add-host project.local:127.0.0.1 --link grid:grid --link hexaa-test-data-manager:hexaa-test-data-manager --link hexaa-backend:hexaa-backend solazs/hexaa-newui
+
+sleep 18
+docker exec -t project.local /var/www/project/test.sh /var/www/project
