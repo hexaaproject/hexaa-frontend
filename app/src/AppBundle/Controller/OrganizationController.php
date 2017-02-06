ae7477910e942061804d886daabe041904c664ba
Annamária Nagy <nagy.annamaria@sztaki.mta.hu>
2017-02-06 13:58:39 +0100

    Merge után

--- app/src/AppBundle/Controller/OrganizationController.php
+++ app/src/AppBundle/Controller/OrganizationController.php
@@ -23,10 +23,11 @@ class OrganizationController extends Controller {
         $organizations = $this->getOrganizations();
         $services = $this->getServices();
         return $this->render(
-                        'AppBundle:Organization:index.html.twig', array(
+            'AppBundle:Organization:index.html.twig',
+                array(
                     'organizations' => $organizations,
                     'services' => $services
-                        )
+                )
         );
     }
 
@@ -34,7 +35,8 @@ class OrganizationController extends Controller {
      * @Route("/addStepOne")
      * @Template()
      */
-    public function addStepOneAction(Request $request) {
+    public function addStepOneAction(Request $request)
+    {
         return $this->render('AppBundle:Organization:addStepOne.html.twig', array());
     }
 
@@ -42,7 +44,8 @@ class OrganizationController extends Controller {
      * @Route("/addStepTwo")
      * @Template()
      */
-    public function addStepTwoAction(Request $request) {
+    public function addStepTwoAction(Request $request)
+    {
         return $this->render('AppBundle:Organization:addStepTwo.html.twig', array());
     }
 
@@ -50,7 +53,8 @@ class OrganizationController extends Controller {
      * @Route("/addStepThree")
      * @Template()
      */
-    public function addStepThreeAction(Request $request) {
+    public function addStepThreeAction(Request $request)
+    {
         return $this->render('AppBundle:Organization:addStepThree.html.twig', array());
     }
 
@@ -58,7 +62,8 @@ class OrganizationController extends Controller {
      * @Route("/addStepFour")
      * @Template()
      */
-    public function addStepFourAction(Request $request) {
+    public function addStepFourAction(Request $request)
+    {
         return $this->render('AppBundle:Organization:addStepFour.html.twig', array());
     }
 
@@ -66,15 +71,18 @@ class OrganizationController extends Controller {
      * @Route("/addStepFive")
      * @Template()
      */
-    public function addStepFiveAction(Request $request) {
+    public function addStepFiveAction(Request $request)
+    {
         return $this->render('AppBundle:Organization:addStepFive.html.twig', array());
     }
+
 
     /**
      * @Route("/addStepSix")
      * @Template()
      */
-    public function addStepSixAction(Request $request) {
+    public function addStepSixAction(Request $request)
+    {
         $id = 1; // TODO, ez az org id lesz, miután rendesen sikeresen perzisztálódott az org, és ezt kaptuk vissza
         return $this->render('AppBundle:Organization:addStepSix.html.twig', array('id' => $id));
     }
@@ -85,12 +93,13 @@ class OrganizationController extends Controller {
     public function showAction($id) {
         $organization = $this->getOrganization($id);
         return $this->render(
-                        'AppBundle:Organization:show.html.twig', array(
-                    'organization' => $organization,
-                    'organizations' => $this->getOrganizations(),
-                    'services' => $this->getServices(),
-                    'orgsubmenubox' => $this->getorgsubmenupoints()
-                        )
+            'AppBundle:Organization:show.html.twig',
+            array(
+                'organization' => $organization,
+                'organizations' => $this->getOrganizations(),
+                'services' => $this->getServices(),
+                'orgsubmenubox' => $this->getorgsubmenupoints()
+            )
         );
     }
 
@@ -98,7 +107,8 @@ class OrganizationController extends Controller {
      * @Route("/properties/{id}")
      * @Template()
      */
-    public function propertiesAction($id) {
+    public function propertiesAction($id)
+    {
         $organization = $this->getOrganization($id);
         $roles = $this->rolesToAccordion($this->getRoles($organization));
         $organization['default_role_name'] = null;
@@ -114,17 +124,18 @@ class OrganizationController extends Controller {
             "Description" => "description",
             "Home page" => "url",
             "Default role" => "default_role_name"
-        );
+            );
 
         return $this->render(
-                        'AppBundle:Organization:properties.html.twig', array(
-                    "organizations" => $this->getOrganizations(),
-                    "services" => $this->getServices(),
-                    "organization" => $organization,
-                    "roles" => $roles,
-                    "propertiesbox" => $propertiesbox,
-                    'orgsubmenubox' => $this->getorgsubmenupoints()
-                        )
+            'AppBundle:Organization:properties.html.twig',
+            array(
+                "organizations" => $this->getOrganizations(),
+                "services" => $this->getServices(),
+                "organization" => $organization,
+                "roles" => $roles,
+                "propertiesbox" => $propertiesbox,
+                'orgsubmenubox' => $this->getorgsubmenupoints()
+            )
         );
     }
 
@@ -132,7 +143,8 @@ class OrganizationController extends Controller {
      * @Route("/users/{id}")
      * @Template()
      */
-    public function usersAction($id) {
+    public function usersAction($id)
+    {
         $organization = $this->getOrganization($id);
         $managers = $this->getManagers($organization);
         $members = $this->getMembers($organization);
@@ -140,57 +152,60 @@ class OrganizationController extends Controller {
             "change_roles" => array(
                 "class" => "btn-blue",
                 "text" => "Change roles"
-            ),
+                ),
             "revoke" => array(
                 "class" => "btn-blue",
                 "text" => "Revoke"
-            ),
+                ),
             "message" => array(
                 "class" => "btn-blue",
                 "text" => "Message"
-            ),
+                ),
             "remove" => array(
                 "class" => "btn-blue",
                 "text" => "Remove"
-            ),
+                ),
             "invite" => array(
-                "class" => "btn-red pull-right",
+                "class" => "btn-red",
+                "last" => true,
                 "text" => '<i class="glyphicon glyphicon-plus"></i> Invite'
-            ),
-        );
+                ),
+            );
         $members_buttons = array(
             "change_roles" => array(
                 "class" => "btn-blue",
                 "text" => "Change roles"
-            ),
+                ),
             "proposal" => array(
                 "class" => "btn-blue",
                 "text" => "Proposal"
-            ),
+                ),
             "message" => array(
                 "class" => "btn-blue",
                 "text" => "Message"
-            ),
+                ),
             "remove" => array(
                 "class" => "btn-blue",
                 "text" => "Remove"
-            ),
+                ),
             "invite" => array(
-                "class" => "btn-red pull-right",
+                "class" => "btn-red",
+                "last" => true,
                 "text" => '<i class="glyphicon glyphicon-plus"></i> Invite'
-            ),
-        );
+                ),
+            );
         return $this->render(
-                        'AppBundle:Organization:users.html.twig', array(
-                    "managers" => $managers,
-                    "members" => $members,
-                    "organization" => $organization,
-                    "organizations" => $this->getOrganizations(),
-                    "services" => $this->getServices(),
-                    "managers_buttons" => $managers_buttons,
-                    "members_buttons" => $members_buttons,
-                    'orgsubmenubox' => $this->getorgsubmenupoints()
-                        )
+            'AppBundle:Organization:users.html.twig',
+            array(
+                "managers" => $managers,
+                "members" => $members,
+                "organization" => $organization,
+                "organizations" => $this->getOrganizations(),
+                "services" => $this->getServices(),
+                "managers_buttons" => $managers_buttons,
+                "members_buttons" => $members_buttons,
+                'orgsubmenubox' => $this->getorgsubmenupoints()
+            )
         );
     }
 
@@ -198,20 +213,22 @@ class OrganizationController extends Controller {
      * @Route("/roles/{id}")
      * @Template()
      */
-    public function rolesAction($id) {
+    public function rolesAction($id)
+    {
         $organization = $this->getOrganization($id);
         $roles = $this->getRoles($organization);
         $roles_accordion = $this->rolesToAccordion($roles);
 
         return $this->render(
-                        'AppBundle:Organization:roles.html.twig', array(
-                    "organization" => $organization,
-                    "organizations" => $this->getOrganizations(),
-                    "roles" => $roles,
-                    "services" => $this->getServices(),
-                    "roles_accordion" => $roles_accordion,
-                    'orgsubmenubox' => $this->getorgsubmenupoints()
-                        )
+            'AppBundle:Organization:roles.html.twig',
+            array(
+                "organization" => $organization,
+                "organizations" => $this->getOrganizations(),
+                "roles" => $roles,
+                "services" => $this->getServices(),
+                "roles_accordion" => $roles_accordion,
+                'orgsubmenubox' => $this->getorgsubmenupoints()
+            )
         );
     }
 
@@ -219,14 +236,16 @@ class OrganizationController extends Controller {
      * @Route("/connectedservices/{id}")
      * @Template()
      */
-    public function connectedservicesAction($id) {
+    public function connectedservicesAction($id)
+    {
         return $this->render(
-                        'AppBundle:Organization:connectedservices.html.twig', array(
-                    "organization" => $this->getOrganization($id),
-                    "organizations" => $this->getOrganizations(),
-                    "services" => $this->getServices(),
-                    'orgsubmenubox' => $this->getorgsubmenupoints()
-                        )
+            'AppBundle:Organization:connectedservices.html.twig',
+            array(
+                "organization" => $this->getOrganization($id),
+                "organizations" => $this->getOrganizations(),
+                "services" => $this->getServices(),
+                'orgsubmenubox' => $this->getorgsubmenupoints()
+            )
         );
     }
 
@@ -241,46 +260,54 @@ class OrganizationController extends Controller {
         return $submenubox;
     }
 
-    private function getOrganization($id) {
+
+    private function getOrganization($id)
+    {
         $client = $this->getUser()->getClient();
         $organization = Organization::get($client, $id);
         return $organization;
     }
 
-    private function getOrganizations() {
+    private function getOrganizations()
+    {
         $client = $this->getUser()->getClient();
         $organization = Organization::cget($client);
         return $organization;
     }
 
-    private function getServices() {
+    private function getServices()
+    {
         $client = $this->getUser()->getClient();
         $organization = Service::cget($client);
         return $organization;
     }
 
-    private function getRoles($organization) {
+    private function getRoles($organization)
+    {
         $verbose = "expanded";
         $roles = Organization::rget($this->getUser()->getClient(), $organization['id'], $verbose);
         return $roles;
     }
 
-    private function getManagers($organization) {
+    private function getManagers($organization)
+    {
         return Organization::managersget($this->getUser()->getClient(), $organization['id']);
     }
 
-    private function getMembers($organization) {
+    private function getMembers($organization)
+    {
         return Organization::membersget($this->getUser()->getClient(), $organization['id']);
     }
 
-    private function rolesToAccordion($roles) {
+    private function rolesToAccordion($roles)
+    {
         $roles_accordion = array();
 
         foreach ($roles as $role) {
             $roles_accordion[$role['id']]['title'] = $role['name'];
             $members = array();
             $permissions = array();
-
+            
             foreach ($role['principals'] as $principal) {
                 $members[] = $principal['principal']['display_name'];
             }
@@ -292,14 +319,15 @@ class OrganizationController extends Controller {
                 array(
                     'key' => 'Permissions',
                     'values' => $permissions
-                ),
+                    ),
                 array(
                     'key' => 'Members',
                     'values' => $members
-                )
-            );
+                    )
+                );
         }
         return $roles_accordion;
     }
 
 }
+
