f4975387a0603b857ec87f633077b9c4aad96a5b
gyufi <szabo.gyula@sztaki.mta.hu>
2017-05-02 21:10:42 +0200

    miért nem talál metadata providert a shibd?
    Change-Id: Ib31163e6beb952c04514f3bcb1c4cc01338144d6

--- app/test.sh
+++ app/test.sh
@@ -3,12 +3,11 @@ if [[ "$#" -lt 1 ]]; then
 	echo "argumentum: mount könyvtár, ahol a behat.yml van."
 fi
 
-cat /var/log/shibboleth/shibd.log
-ls -la  /etc/shibboleth/metadata/metadata.xml
-
 ROOT_DIR=$1
 shift
 rm -rf /tmp/cache/prod
 rm -rf /tmp/cache/dev
 rm -rf /tmp/cache/test
 $ROOT_DIR/vendor/bin/behat -c $ROOT_DIR/behat.yml $*
+
+cat /var/log/shibboleth/shibd.log
