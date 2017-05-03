e3798d57596baf245e6f103d257c1ac96e491b76
gyufi <szabo.gyula@sztaki.mta.hu>
2017-05-03 10:56:54 +0200

    deploying gitlab-ci pimp még
    Change-Id: I498afe94745773a7ae1b710a6ff20c8deaa89bd0

--- app/test.sh
+++ app/test.sh
@@ -9,5 +9,9 @@ rm -rf /tmp/cache/prod
 rm -rf /tmp/cache/dev
 rm -rf /tmp/cache/test
 $ROOT_DIR/vendor/bin/behat -c $ROOT_DIR/behat.yml $*
+last_exit=$?
+if [[ $last_exit != 0 ]]; then
+    cat /var/log/shibboleth/shibd.log
+    exit $rc
+fi
 
-cat /var/log/shibboleth/shibd.log
