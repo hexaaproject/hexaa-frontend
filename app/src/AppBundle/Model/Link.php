d0db19bd1e4cc96d510bdd63d0c287d64e544954
Nagy Annamária <nagy.annamaria@sztaki.mta.hu>
2017-07-11 12:59:00 +0200

    Line separator megváltoztatása \n-re \r\n helyett hogy a phpcs ne error-ozzon

--- app/src/AppBundle/Model/Link.php
+++ app/src/AppBundle/Model/Link.php
@@ -1,22 +1,23 @@
 <?php
-namespace AppBundle\Model;
-
-/**
- * Class Link
- * @package AppBundle\Model
- */
-class Link extends AbstractBaseResource
-{
-    protected $pathName = 'links';
+namespace AppBundle\Model {
 
     /**
-     * Generate new link token
-     *
-     * @param string $id ID of link
-     * @return array
+     * Class Link
+     * @package AppBundle\Model
      */
-    public function getNewLinkToken(string $id): array
+    class Link extends AbstractBaseResource
     {
-        return $this->getCollection($this->pathName.'/'.$id.'/token');
+        protected $pathName = 'links';
+
+        /**
+         * Generate new link token
+         *
+         * @param string $id ID of link
+         * @return array
+         */
+        public function getNewLinkToken(string $id): array
+        {
+            return $this->getCollection($this->pathName.'/'.$id.'/token');
+        }
     }
 }
