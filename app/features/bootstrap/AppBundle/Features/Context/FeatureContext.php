a2eaefca27450edeab671520f5680e8538a82a0f
gyufi <szabo.gyula@sztaki.mta.hu>
2017-07-13 14:27:31 +0200

    delete an organization
    Change-Id: I96d8b2403dbd0631f6e5726e1b8fe18a93fa07e3

--- app/features/bootstrap/AppBundle/Features/Context/FeatureContext.php
+++ app/features/bootstrap/AppBundle/Features/Context/FeatureContext.php
@@ -122,4 +122,19 @@ class FeatureContext extends MinkContext
             $findName->click();
         }
     }
+
+    /**
+     * @When I click the :arg1 element
+     */
+    public function iClickTheElement($selector)
+    {
+        $page = $this->getSession()->getPage();
+        $element = $page->find('css', $selector);
+
+        if (empty($element)) {
+            throw new \Exception("No html element found for the selector ('$selector')");
+        }
+
+        $element->click();
+    }
 }
