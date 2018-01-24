13ea62641a1d72e6aae2446b9b1d69cb731790f3
Nagy Annamária <nagy.annamaria@sztaki.mta.hu>
2018-01-24 10:38:36 +0100

    Organization lecsupaszítva memberre

--- app/src/AppBundle/Controller/OrganizationController.php
+++ app/src/AppBundle/Controller/OrganizationController.php
@@ -248,7 +248,7 @@ class OrganizationController extends BaseController
                     'services' => $this->get('service')->cget(),
                     "admin" => $this->get('principal')->isAdmin()["is_admin"],
                     'submenu' => 'true',
-                    'ismanager' => $this->isManager($id),
+                    'ismanager' => $manager
                 )
             );
         } else {
@@ -263,7 +263,7 @@ class OrganizationController extends BaseController
                     'services' => $this->get('service')->cget(),
                     "admin" => $this->get('principal')->isAdmin()["is_admin"],
                     'submenu' => 'true',
-                    'ismanager' => $this->isManager($id),
+                    'ismanager' => $manager,
                 )
             );
         }
