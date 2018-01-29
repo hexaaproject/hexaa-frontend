e9c4bb4dd976a956721b52ef1b40aaba14eb7ed5
Soltész Balázs <solazs@sztaki.hu>
2018-01-29 15:45:31 +0100

    Improve and fix errors in new docker env.
    Change-Id: I412c7b6fa836e39fb1912e1c73b214efa80ca986

--- docker-prod/boot.sh
+++ docker-prod/boot.sh
@@ -10,20 +10,13 @@ set -e
 HEXAA_FRONTEND_SECRET=${HEXAA_FRONTEND_SECRET:-"ThisTokenIsNotSoSecretChangeIt"}
 HEXAA_FRONTEND_SHIB_USERNAME_ATTRIBUTE=${HEXAA_FRONTEND_SHIB_USERNAME_ATTRIBUTE:-"eduPersonPrincipalName"}
 HEXAA_FRONTEND_SHIB_MODULE_ATTRIBUTE=${HEXAA_FRONTEND_SHIB_MODULE_ATTRIBUTE:-"HTTP_SHIB_APPLICATION_ID"}
-HEXAA_FRONTEND_SHIB_ATTRIBUTE_MAP=${HEXAA_FRONTEND_SHIB_ATTRIBUTE_MAP:-"
-        eppn: eduPersonPrincipalName
-        displayName: displayName
-        email: mail"}
+HEXAA_FRONTEND_SHIB_ATTRIBUTE_MAP=${HEXAA_FRONTEND_SHIB_ATTRIBUTE_MAP:-"\n        eppn: eduPersonPrincipalName\n        displayName: displayName\n        email: mail"}
 HEXAA_FRONTEND_SHIB_LOGOUT_RETURN_PATH=${HEXAA_FRONTEND_SHIB_LOGOUT_RETURN_PATH:-"homepage"}
 # note: add "/" at the end of the URL!
-HEXAA_FRONTEND_API_BASE_URL=${HEXAA_FRONTEND_BASE_URL:-"http://hexaa-backend/api/"}
+HEXAA_FRONTEND_API_BASE_URL=${HEXAA_FRONTEND_API_BASE_URL:-"http://hexaa-backend/api/"}
 HEXAA_FRONTEND_API_PERMISSION_PREFIX=${HEXAA_FRONTEND_API_PERMISSION_PREFIX:-"some:entitlement:prefix:hexaa"}
 HEXAA_FRONTEND_API_SCOPED_KEY=${HEXAA_FRONTEND_API_SCOPED_KEY:-"InsertSomeSecretHere"}
-HEXAA_FRONTEND_INVITATION_CONFIG=${HEXAA_FRONTEND_INVITATION_CONFIG:-"
-        subject: \"[HEXAA] invitation\"
-        from: \"no_reply@hexaa.eduid.hu\"
-        reply-to: \"no_reply@hexaa.eduid.hu\"
-        footer: \"sincerely: hexaa.eduid.hu\""}
+HEXAA_FRONTEND_INVITATION_CONFIG=${HEXAA_FRONTEND_INVITATION_CONFIG:-"\n        subject: \"[HEXAA] invitation\"\n        from: \"no_reply@hexaa.eduid.hu\"\n        reply-to: \"no_reply@hexaa.eduid.hu\"\n        footer: \"sincerely: hexaa.eduid.hu\""}
 # Mailer parameters
 HEXAA_FRONTEND_MAILER_TRANSPORT=${HEXAA_FRONTEND_MAILER_TRANSPORT:-"smtp"}
 HEXAA_FRONTEND_MAILER_HOST=${HEXAA_FRONTEND_MAILER_HOST:-"smtp"}
@@ -35,12 +28,11 @@ HEXAA_FRONTEND_LOG_TO_STDERR=${HEXAA_FRONTEND_LOG_TO_STDERR:-"true"}
 
 # Copy alternative logging config and clear cache IF configured to do so
 if [ "$HEXAA_FRONTEND_LOG_TO_STDERR" = "true" ]; then
-    cp /root/config_prod.yml /opt/hexaa-frontend/app/config/config_prod.yml
-    rm -rf /opt/hexaa-frontend/app/cache/*
+    cp /root/config_prod.yml /opt/hexaa-newui/app/config/config_prod.yml
 fi
 
 # Write parameters.yml
-cat >/opt/hexaa-newui/app/config/parameters.yml <<EOF
+HEXAA_FRONTEND_PARAMETERS_YML=$(cat <<EOF
 parameters:
     secret:                       $HEXAA_FRONTEND_SECRET
 
@@ -60,5 +52,17 @@ parameters:
     mailer_password:   $HEXAA_FRONTEND_MAILER_PASSWORD
 
 EOF
+)
+
+echo "$HEXAA_FRONTEND_PARAMETERS_YML" > /opt/hexaa-newui/app/config/parameters.yml
+
+if [ ! -f /opt/hexaa-newui.deployed ]; then
+    # dump css
+    #su www-data -m -s /bin/sh -c "/usr/local/bin/php /opt/hexaa-newui/bin/console assetic:dump"
+    php /opt/hexaa-newui/bin/console assetic:dump --env=prod --no-debug
+fi
+
+# Clear Symfony cache az startup
+rm -rf /opt/hexaa-newui/var/cache/*
 
 docker-php-entrypoint php-fpm
