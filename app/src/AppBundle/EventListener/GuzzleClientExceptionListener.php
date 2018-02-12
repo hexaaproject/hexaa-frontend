01010eab24c0aff66260ccf45401be5776860068
Szabó Gyula <szabo.gyula@sztaki.mta.hu>
2018-02-12 11:04:17 +0100

    add templating to listener to render the error

--- app/src/AppBundle/EventListener/GuzzleClientExceptionListener.php
+++ app/src/AppBundle/EventListener/GuzzleClientExceptionListener.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AppBundle\EventListener;
+use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 
@@ -12,11 +13,18 @@ use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
  */
 class GuzzleClientExceptionListener
 {
+    private $templating;
+
+    public function __construct(TwigEngine $templating)
+    {
+        $this->templating = $templating;
+    }
+
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $exception = $event->getException();
-        $response = new Response();
 
+        $response = new Response($this->templating->render('AppBundle:Errors:GuzzleClientException.html.twig'));
         $event->setResponse($response);
     }
 }
