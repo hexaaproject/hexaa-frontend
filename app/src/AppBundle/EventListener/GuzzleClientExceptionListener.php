53482eed789bec7e8a45eb024be444c977cb4a4f
Szabó Gyula <szabo.gyula@sztaki.mta.hu>
2018-02-12 11:51:59 +0100

    add error template

--- app/src/AppBundle/EventListener/GuzzleClientExceptionListener.php
+++ app/src/AppBundle/EventListener/GuzzleClientExceptionListener.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace AppBundle\EventListener;
+
+use GuzzleHttp\Exception\ClientException;
 use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Created by PhpStorm.
@@ -14,17 +17,54 @@ use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 class GuzzleClientExceptionListener
 {
     private $templating;
+    private $router;
 
-    public function __construct(TwigEngine $templating)
+
+    public function __construct(TwigEngine $templating, RouterInterface $router)
     {
         $this->templating = $templating;
+        $this->router = $router;
+
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
+        /** @var ClientException $exception */
         $exception = $event->getException();
 
-        $response = new Response($this->templating->render('AppBundle:Errors:GuzzleClientException.html.twig'));
-        $event->setResponse($response);
+        if ($exception instanceof ClientException) {
+
+            $header = 'Error';
+            $body = 'Some error...';
+            $redirectUrl = $this->router->generate("homepage");
+            $catched = false;
+
+            switch ($exception->getCode()) {
+                case 404:
+                    $header = "Resource not found.";
+                    $body = "The referred resource not found on backend.";
+                    $catched = true;
+                    break;
+                case 403:
+                    $header = "Access denied.";
+                    $body = "You dont have access to the resource.";
+                    $catched = true;
+                    break;
+            }
+
+            if ($catched) {
+                $response = new Response(
+                    $this->templating->render(
+                        'AppBundle:Errors:GuzzleClientException.html.twig',
+                        array(
+                            'header'      => $header,
+                            'body'        => $body,
+                            'redirectUrl' => $redirectUrl,
+                        )
+                    )
+                );
+                $event->setResponse($response);
+            }
+        }
     }
 }
